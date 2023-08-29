aa6c9454ba9507601fc25f503873bb042032de8e
thasneem <thasneem9mt@gmail.com>
2023-08-29 14:54:44 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-$('li').on('click',function(){
+$('ul').on('click',"li",function(){
     $(this).toggleClass('blackC')
 })
 
-$('span').on('click', function(fadeT){
+
+$('ul').on('click','span', function(fadeT){
     $(this).parent().fadeOut(1000,function(){
         $(this).remove()
     })
     fadeT.stopPropagation()
 })
+
+
+$('input').on('keypress',function(event){
+    if(event.which === 13){
+        // console.log(event.target.value);
+        let addtodo = $(this).val()
+        $(this).val('')
+        $('ul').append(`<li><span><i class="fa-regular fa-circle-xmark"></i></span> ${addtodo}</li>`)
+
+    }
+})
+
